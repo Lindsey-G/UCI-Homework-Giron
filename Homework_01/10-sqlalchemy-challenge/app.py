610064a5e5807b9ab0b1a577944e157466ba6f34
Lindsey G <lgiron84@gmail>
2020-11-17 18:09:28 -0800

    app.py update

--- Homework_01/10-sqlalchemy-challenge/app.py
+++ Homework_01/10-sqlalchemy-challenge/app.py
@@ -3,9 +3,14 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/api/v1.0/precipitation")
+def precipitation():
 
+    return jsonify()
+    
 @app.route("/api/v1.0/stations")
+def stations():
 
+    return jsonify()
 
 @app.route("")
 /api/v1.0/<start> 
