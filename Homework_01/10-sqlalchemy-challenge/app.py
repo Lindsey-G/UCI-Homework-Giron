278d412e504ceff4a774cdab668fae91445d880c
Lindsey G <lgiron84@gmail>
2020-11-17 17:01:44 -0800

    app.py

---
+++ Homework_01/10-sqlalchemy-challenge/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/api/v1.0/precipitation")
+
+@app.route("/api/v1.0/stations")
+
+
+@app.route("")
+/api/v1.0/<start> 
+
+@app.route
+/api/v1.0/<start>/</end>
+
+if ___name__ == "__main__":
+    app.run(debug=True)
