e4e2cacf30f68c3387474cfb506017d3d48b3bf7
Lindsey G <lgiron84@gmail>
2021-01-08 16:59:55 -0800

    update

--- Homework_01/16-D3-Challenge/D3-data-journalism/assets/js/app.js
+++ Homework_01/16-D3-Challenge/D3-data-journalism/assets/js/app.js
@@ -38,6 +38,13 @@ function xScale(newsData, chosenXAxis) {
         .range([0, width]);
     return xLinearScale;
 }
+var chosenYAxis = "poverty"
+function yScale(newsData, chosenYAxis) {
+    var yLinearScale = d3.scaleLinear()
+        .domian([0, d3.max(newsData, d => d[chosenYAxis])])
+        .range([height, 0]);
+    return yLinearScale;
+}
 // Create function to update new selected xAxis when clicked on x label 
 function renderAxes(newXScale, xAxis) {
     var bottomAxis = d3.axisBottom(newXScale);
@@ -52,12 +59,84 @@ function renderAxes(newXScale, xAxis) {
 function renderCircles(circlesGroup, newXScale, chosenXAxis) {
     circlesGroup.transition()
         .duration(1000)
-        .call();
+        .attr("c", d => newXScale(d[chosenXAxis]));
     
     return circlesGroup;
 }
+
+function updateToolTip(chosenXAxis, circlesGroup) {
+    var label;
+    if (chosenXAxis === "obesity") {
+        label = "obesity";
+    }
+    else {
+        label = "poverty";
+    }
+
+    var toolTip = d3.tip()
+        .attr("class", "toolTip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`${d.abbr}<br>${label}${d[chosenXAxis]}`);
+        });
+
+    circlesGroup.call(toolTip);
+
+    circlesGroup.on("mouseover", function(data) {
+        toolTip.show(data);
+    })
+        .on("mouseout", function(data, index) {
+            toolTip.hide(data);
+        });
+    
+    return circlesGroup;
+}
+
 // Read in csv
-d3.csv("/assets/data/data.csv").then(function(newsData) {
-    console.log(newsData);
+d3.csv("/assets/data/data.csv").then(function(newsData, err) {
+    if (err) throw err;
+
+    newsData.forEach(function(data) {
+        data.obesity = +data.obesity;
+        data.poverty = +data.poverty;
+        // console.log(data.obesity);
+        // console.log(data.poverty);
+    })
+    var xLinearScale = xScale(newsData, chosenXAxis);
+    var yLinearScale = yScale(newsData, chosenYAxis);
+
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    var xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    var circlesGroup = chartGroup.selectAll("cirlce")
+        .data(newsData)
+        .enter()
+        .append("cirlce")
+        .attr("cx", d => xLinearScale(d[chosenXAxis]))
+        .attr("cy", d => yLinearScale(d[chosenYAxis]))
+        .attr("r", 20)
+        .attr("fill", "purple")
+        .attr("opacity", ".5");
+
+    var labelsGroup = chartGroup.append("g")
+        .attr("transform", `translate(${width / 2}${height + 20 })`);
+    
+    var obesityData = labelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 20)
+        .attr("value", "obesity")
+        .classed("")
+
+
 }); 
+
+
 
