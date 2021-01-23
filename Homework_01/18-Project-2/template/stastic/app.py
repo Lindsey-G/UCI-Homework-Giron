c56bb9974a783689104385940c5a3496b19ed3a5
Lindsey G <lgiron84@gmail>
2021-01-23 12:49:03 -0800

    flask connection with postgres working

---
+++ Homework_01/18-Project-2/template/stastic/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect
+
+# Initiate Flask app
+app = Flask(__name__)
+
+# create connection to pgadmin
+
+
+@app.route("/")
+def homepage():
+
+@app.route("/api/v1.0/calendar")
+def calendar():
+
+@app.route("/api/v1.0/map")
+def map():
+
+@app.route("/api/v1.0/scatter")
+def scatter():
+
+@app.route("/api/v1.0/")
+
+
+
+# Run app
+if __name__ == "__main__":
+    app.run(debug=True) 
